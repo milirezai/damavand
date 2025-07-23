315543e99cdb5412788475720e4dce96c13b8e5f
milirezai <miladrezaix@gmail.com>
2025-07-23 19:51:12 +0330

    Implementation of view system management

---
+++ system/View/ViewBuilder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace System\View;
+
+use App\Providers\AppServiceProvider;
+use System\View\Traits\HasViewLoader;
+use System\View\Traits\HasExtendsContent;
+use System\View\Traits\HasIncludeContent;
+use Exception;
+
+class ViewBuilder
+{
+
+    use HasViewLoader,HasExtendsContent,HasIncludeContent;
+
+    public $content;
+    public $vars = [];
+    public function run($dir)
+    {
+        $this->content = $this->viewLoader($dir);
+        $this->checkExtendsContent();
+        $this->checkIncludeContent();
+        Composer::setViews($this->viewNameArray);
+        $appServiceProvider= new AppServiceProvider();
+        $appServiceProvider->boot();
+        $this->vars = Composer::getVars;
+    }
+
+}
